e138b6020103e76b5b1a76760df12b49a410bbe6
Urban Vidovič <urbanfoundit@gmail.com>
2021-12-03 14:10:11 +0100

    Init COA project

---
+++ src/main/java/COABenchmark.java
@@ -0,0 +1,32 @@
+import org.um.feri.ears.algorithms.Algorithm;
+import org.um.feri.ears.algorithms.so.abc.ABC;
+import org.um.feri.ears.algorithms.so.gwo.GWO;
+import org.um.feri.ears.algorithms.so.jade.JADE;
+import org.um.feri.ears.algorithms.so.random.RandomWalkAlgorithm;
+import org.um.feri.ears.algorithms.so.tlbo.TLBOAlgorithm;
+import org.um.feri.ears.benchmark.Benchmark;
+import org.um.feri.ears.benchmark.RPUOed30Benchmark;
+import org.um.feri.ears.util.Util;
+
+import java.util.ArrayList;
+
+public class COABenchmark {
+    public static void main(String[] args) {
+        Util.rnd.setSeed(System.currentTimeMillis()); //set the seed of the random generator
+        Benchmark.printInfo = false; //prints one on one results
+        //add algorithms to a list
+        ArrayList<Algorithm> algorithms = new ArrayList<Algorithm>();
+        algorithms.add(new Coyote());
+//        algorithms.add(new ABC());
+//        algorithms.add(new GWO());
+//        algorithms.add(new TLBOAlgorithm());
+//        algorithms.add(new RandomWalkAlgorithm());
+//        algorithms.add(new JADE());
+
+        RPUOed30Benchmark rpuoed30 = new RPUOed30Benchmark(); // benchmark with prepared tasks and settings
+
+        rpuoed30.addAlgorithms(algorithms);  // register the algorithms in the benchmark
+
+        rpuoed30.run(10); //start the tournament with 10 runs/repetitions
+    }
+}
---
+++ src/main/java/Coyote.java
@@ -0,0 +1,36 @@
+import org.um.feri.ears.algorithms.Algorithm;
+import org.um.feri.ears.algorithms.AlgorithmInfo;
+import org.um.feri.ears.algorithms.Author;
+import org.um.feri.ears.problems.DoubleSolution;
+import org.um.feri.ears.problems.StopCriterionException;
+import org.um.feri.ears.problems.Task;
+
+public class Coyote extends Algorithm {
+    private DoubleSolution best; // used to save global best solution
+
+    public Coyote() {
+        ai = new AlgorithmInfo("COA", "Coyote Optimization Algorithm", ""); // add algorithm name
+        au = new Author("kenny", "N/A"); // add author information
+    }
+
+    @Override
+    public DoubleSolution execute(Task task) throws StopCriterionException { // method which executes the algorithm
+        // the task object holds information about the stopping criterion
+        // and information about the problem (number of dimensions, number of constraints, upper and lower bounds...)
+        DoubleSolution newSolution;
+        best = task.getRandomEvaluatedSolution();  // generate new random solution (number of evaluations is incremented automatically)
+        // to evaluate a custom solution or an array use task.eval(mySolution)
+        while (!task.isStopCriterion()) { // run until the stopping criterion is not reached
+
+            newSolution = task.getRandomEvaluatedSolution();
+            if (task.isFirstBetter(newSolution, best)) { // use method isFirstBetter to check which solution is better (it checks constraints and considers the type of the problem (minimization or maximization))
+                best = newSolution;
+            }
+            task.incrementNumberOfIterations(); // increase the number of generations for each iteration of the main loop
+        }
+        return best; // return the best solution found
+    }
+
+    @Override
+    public void resetToDefaultsBeforeNewRun() {}
+}
